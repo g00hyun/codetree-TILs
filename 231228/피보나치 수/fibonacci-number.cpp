53e21bce8e3ceadd8abaf9e441a80e06504bfcb6
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2023-12-28 16:23:12 +0900

    Title: 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/피보나치 수/fibonacci-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int dp[46];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    dp[1] = dp[2] = 1;
+    for(int i = 3; i<=n; i++)
+        dp[i] = dp[i-1] + dp[i-2];
+    
+    cout << dp[n];
+    return 0;
+}

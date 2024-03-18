be2ea87c63bc90104fdab849dbdedd0b6497bcab
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2024-03-18 16:31:07 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240318/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240318/사각형 채우기 3/rectangle-fill-3.cpp
@@ -11,8 +11,7 @@ int main() {
 
     dp[0] = 1;
     dp[1] = 2;
-    dp[2] = 7;
-    for(int i = 3; i<=n; i++) {
+    for(int i = 2; i<=n; i++) {
         dp[i] = (2 * dp[i-1] + 3 * dp[i-2]) % 1000000007;
         for(int j = i-3; j>=0; j--)
             dp[i] = (dp[i] + dp[j] * 2) % 1000000007;

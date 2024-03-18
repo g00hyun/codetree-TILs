9ad08e2997a986d16601634526e4dae5d305eaac
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2024-03-18 16:16:02 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3; i<=n; i++)
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+
+    cout << dp[n];
+
+    return 0;
+}

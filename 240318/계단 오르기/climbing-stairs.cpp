6e01f5451de6ee4cb2e9c5f849b9f23b4c426183
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2024-03-18 15:52:30 +0900

    Title: 계단 오르기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    dp[2] = 1;
+    dp[3] = 1;
+    for(int i = 4; i<=n; i++) {
+        dp[i] = (max(dp[i-2], dp[i-3]) + 1) % 10007;
+    }
+
+    cout << dp[n];
+    return 0;
+}

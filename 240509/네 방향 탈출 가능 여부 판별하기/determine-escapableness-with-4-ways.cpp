302e04509d1f184721e6285026bedeba8c85660f
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2024-05-09 20:13:24 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 14ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240509/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
+++ 240509/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -15,7 +15,7 @@ bool inRange(int x, int y) {
 
 bool Cango(int x, int y) {
     if(!inRange(x,y)) return false;
-    if(visited[x][y]) return false;
+    if(visited[x][y] || arr[x][y] == 0) return false;
     return true;
 }
 

1d4c125859d0e87715f70687877b3861265ec572
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2024-05-28 14:54:52 +0900

    Title: 대응되는 수와 문자, Time: 1008ms, Memory: 5MB, Status: Time Limit Exceed - CodeTree

---
+++ 240528/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int n, m;
+unordered_map<string, int> un_m;
+string sarr[100001];
+
+bool isNumber(const string& str) {
+  return str.find_first_not_of("0123456789") == string::npos;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+
+    for(int i = 1; i<=n; i++)
+        cin >> sarr[i];
+
+    for(int i = 1; i<=n; i++)
+        un_m[sarr[i]] = i;
+
+    for(int i = 1; i<=m; i++) {
+        string tmp;
+        cin >> tmp;
+
+        if(isNumber(tmp)) {
+            for(int i = 1; i<=n; i++)
+                if(stoi(tmp) == un_m[sarr[i]])
+                    cout << sarr[i] << endl;
+        }
+        else
+            cout << un_m[tmp] << endl;
+    }
+    return 0;
+}

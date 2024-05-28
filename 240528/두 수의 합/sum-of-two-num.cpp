427291077c3166fcd7d6bed521de53a51eec8515
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2024-05-28 16:52:32 +0900

    Title: 두 수의 합, Time: 94ms, Memory: 3MB, Status: Passed - CodeTree

--- 240528/두 수의 합/sum-of-two-num.cpp
+++ 240528/두 수의 합/sum-of-two-num.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 unordered_map<int, int> un_m;
 int n,k;
-int arr[100000];
 
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n >> k;
-    for(int i = 0; i<n; i++)
-        cin >> arr[i];
+    for(int i = 0; i<n; i++) {
+        int tmp;
+        cin >> tmp;
+
+        un_m[tmp]++;
+    }
+
+    int result = 0;
+    for(auto it = un_m.begin(); it != un_m.end(); it++) {
+        int v1 = it->first;
+        int v2 = k - v1;
+
+        if(v1 == v2)
+            result += it->second * (it->second -1);
+        else if(un_m.find(v2) != un_m.end())
+            result += it->second * un_m[v2];
+    }
     
-    for(int i = 0; i<n-1; i++)
-        for(int j = i+1; j<n; j++)
-            un_m[arr[i] + arr[j]]++;
-    
-    cout << un_m[k];
+    cout << result/2;
     return 0;
 }

825c4ee56df67d11843ad0ce7f0d3ee88c6203a4
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2024-05-29 02:53:59 +0900

    Title: 자주 등장한 top k 숫자, Time: 65ms, Memory: 3MB, Status: Passed - CodeTree

--- 240529/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
+++ 240529/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
@@ -8,7 +8,12 @@ using namespace std;
 unordered_map<int, int> un_m;
 int n, k;
 int maximum = 0;
-vector<int> v;
+
+bool cmp(pair<int,int> a, pair<int,int> b) {
+    if(a.second == b.second)
+        return a.first > b.first;
+    return a.second > b.second;
+}
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -17,20 +22,13 @@ int main() {
         int tmp;
         cin >> tmp;
 
-        maximum = max(maximum, ++un_m[tmp]);
+        un_m[tmp]++;
     }
 
-    while(k--) {
-        for(auto it = un_m.begin(); it != un_m.end(); it++) {
-            if(maximum == it->second) {
-                v.push_back(it->first);
-                un_m.erase(it->first);
-                break;
-            }
-        }
-    }
+    vector<pair<int,int>> v(un_m.begin(), un_m.end());
+    sort(v.begin(), v.end(), cmp);
 
-    for(auto i : v)
-        cout << i << ' ';
+    for(int i = 0; i<k; i++)
+        cout << v[i].first << ' ';
     return 0;
 }

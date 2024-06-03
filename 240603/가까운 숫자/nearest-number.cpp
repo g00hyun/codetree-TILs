8779181ff5aa5ac4fab7864fec3a4f8ade650554
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2024-06-03 16:33:20 +0900

    Title: 가까운 숫자, Time: 137ms, Memory: 2MB, Status: Wrong Answer - CodeTree

---
+++ 240603/가까운 숫자/nearest-number.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+set<int> s;
+int minimum;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 0; i<n; i++) {
+        int tmp;
+        cin >> tmp;
+
+        if(i == 0)
+            minimum = tmp;
+        else {
+            auto it = s.lower_bound(tmp);
+            
+            if(it != s.end()) {
+                minimum = min({abs(*it) - tmp, abs(*(--it) - tmp), minimum});
+            }
+            else {
+                it--;
+                minimum = min(abs((*it) - tmp), minimum);
+            }
+
+        }
+        
+        cout << minimum << endl;
+
+        s.insert(tmp);
+    }
+    return 0;
+}

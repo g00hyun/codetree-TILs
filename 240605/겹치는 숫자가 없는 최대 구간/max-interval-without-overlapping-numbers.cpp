7ebbfeb6d0fd55165d431813ec1ed83af9a90bd7
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2024-06-05 18:42:40 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 37ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240605/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int arr[100000];
+bool visited[100000];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i = 0; i<n; i++)
+        cin >> arr[i];
+
+    int result = 0;
+    int j = 0;
+    for(int i = 0; i<n; i++) {
+        while(j < n && !visited[arr[j]]) {
+            visited[arr[j]] = true;
+            j++;
+        }
+
+        result = max(result, j - i);
+
+        visited[arr[i]] = false;
+    }
+
+    cout << result;
+    
+    return 0;
+}

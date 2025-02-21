62162ab09c2ffcd99ef7c9d857ac727b279d3de3
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-02-21 14:31:53 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 1000ms, Memory: 160MB, Status: Memory Limit Exceed - CodeTree

---
+++ 250221/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.js
@@ -0,0 +1,80 @@
+class Queue {
+    constructor() {
+        this.q = []
+        this.head = -1
+        this.tail = -1
+    }
+
+    empty() {
+        return this.head === this.tail
+    }
+
+    push(e) {
+        this.q.push(e)
+        this.tail++
+    }
+
+    pop() {
+        if(this.empty())
+            throw new Error("Queue is empty")
+        return this.q[++this.head]
+    }
+
+    front() {
+        if(this.empty())
+            throw new Error("Queue is empty")
+        return this.q[this.head + 1]
+    }
+
+    size() {
+        return this.tail - this.head
+    }
+}
+
+const fs = require('fs')
+const n = Number(fs.readFileSync(0).toString().trim())
+
+const minus1 = (v) => v-1
+const plus1 = (v) => v+1
+const div2 = (v) => Math.trunc(v/2)
+const div3 = (v) => Math.trunc(v/3)
+
+const method = {
+    0: minus1,
+    1: plus1,
+    2: div2,
+    3: div3
+}
+
+const canPM1 = (v) => true
+const canDiv2 = (v) => v%2 === 0
+const canDiv3 = (v) => v%3 === 0
+
+const canMethod = {
+    0: canPM1,
+    1: canPM1,
+    2: canDiv2,
+    3: canDiv3
+}
+
+const q = new Queue()
+
+const BFS = () => {
+    while(!q.empty()) {
+        const [number, cnt] = q.pop()
+
+        if(number === 1) return cnt
+
+        for(let i = 0; i<4; i++) {
+            if(canMethod[i](number))
+                q.push([method[i](number), cnt + 1])
+        }
+    }
+
+    return -1
+}
+
+q.push([n, 0])
+const result = BFS()
+
+console.log(result)

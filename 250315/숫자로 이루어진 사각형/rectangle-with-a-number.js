9551b401726a8b77542fad8513fcbfffe2fd465a
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-03-15 23:58:42 +0900

    Title: 숫자로 이루어진 사각형, Time: 169ms, Memory: 14MB, Status: Runtime Error - CodeTree

---
+++ 250315/숫자로 이루어진 사각형/rectangle-with-a-number.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const N = Number(input[0]);
+// Please write your code here.
+
+let a = 1
+for(let i = 0; i<n; i++)
+    for(let j = 0; j<n; j++) {
+        console.log(a )
+    }

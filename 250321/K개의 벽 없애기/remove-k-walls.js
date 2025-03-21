4b5119e718c435b40000871f2a0a7b7332cb98e7
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-03-21 19:35:22 +0900

    Title: K개의 벽 없애기, Time: 212ms, Memory: 15MB, Status: Passed - CodeTree

--- 250321/K개의 벽 없애기/remove-k-walls.js
+++ 250321/K개의 벽 없애기/remove-k-walls.js
@@ -1,3 +1,26 @@
+class Queue {
+    constructor () {
+        this.q = []
+        this.head = -1
+        this.tail = -1
+    }
+
+    push(e) {
+        this.q.push(e);
+        this.tail++;
+    }
+
+    pop() {
+        if(this.empty())
+            throw new Error('Queue is Empty');
+        return this.q[++this.head];
+    }
+
+    empty() {
+        return this.tail === this.head;
+    }
+}
+
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
@@ -7,7 +30,7 @@ const [r1, c1] = input[1 + n].split(' ').map(Number);
 const [r2, c2] = input[2 + n].split(' ').map(Number);
 
 // Please Write your code here.
-const q = [];
+const q = new Queue();
 const visited = Array(n).fill().map(v => Array(n).fill().map(v => Array(k+1).fill(false)));
 const time = Array(n).fill().map(v => Array(n).fill(-1))
 
@@ -20,8 +43,8 @@ const CanGo = (x,y,bricks) => {
 const BFS = () => {
     const dx = [1,-1,0,0], dy = [0,0,1,-1];
 
-    while(q.length > 0) {
-        const [x,y,bricks] = q.shift();
+    while(!q.empty()) {
+        const [x,y,bricks] = q.pop();
 
         if(x === r2-1 && y === c2-1){
             return time[x][y];

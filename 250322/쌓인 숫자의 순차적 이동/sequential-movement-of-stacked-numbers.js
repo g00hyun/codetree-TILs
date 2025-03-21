42f88260a99a3fa3feb58fea845ddc74efd10da0
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-03-22 01:17:47 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 172ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250322/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.js
+++ 250322/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.js
@@ -23,10 +23,6 @@ const findMoveNode = (x,y) => {
             }
         }
     }
-
-    if(maxval === -1)
-        return [-1,-1]
-
     const nx = x + dx[index], ny = y + dy[index];
     return [nx,ny];
 }
@@ -44,24 +40,45 @@ for(let i = 0; i<n; i++)
     for(let j = 0; j<n; j++)
         grid[i][j] = [grid[i][j]]
 
-moveNums.forEach(v => {
-    let firstmove = true;
+// moveNums.forEach(v => {
+//     let firstmove = true;
 
-    for(let i = 0; i<n; i++)
-        for(let j = 0; j<n; j++)
-            if(firstmove && grid[i][j].includes(v)) {
-                const [nx,ny] = findMoveNode(i,j);
-                if(!(nx === -1 && ny === -1))
-                    Move(i,j,nx,ny,v);
-                firstmove = false;
-            }
-    // console.log(grid)
-})
+//     for(let i = 0; i<n; i++)
+//         for(let j = 0; j<n; j++)
+//             if(firstmove && grid[i][j].includes(v)) {
+//                 const [nx,ny] = findMoveNode(i,j);
+//                 if(grid[nx][ny].length > 0)
+//                     Move(i,j,nx,ny,v);
+//                 firstmove = false;
+//             }
+//     // console.log(grid)
+// })
+
+// console.log(grid)
 
 
-grid.forEach(list => list.forEach(v => {
-    if(v.length === 0)
-        console.log('None')
-    else
-        console.log(v.reverse().join(' '))
-}))
+// grid.forEach(list => list.forEach(v => {
+//     if(v.length === 0)
+//         console.log('None')
+//     else
+//         console.log(v.reverse().join(' '))
+// }))
+
+const a = [1,2,3,4,5]
+const b = [6,7,8,9,0]
+
+const index = a.indexOf(3);
+const sliceArr = a.slice(index);
+
+b.push(...sliceArr);
+a.splice(index);
+
+console.log(a,b)
+console.log()
+const index2 = b.indexOf(3);
+const sliceArr2 = b.slice(index);
+
+a.push(...sliceArr2);
+b.splice(index2);
+
+console.log(a,b)

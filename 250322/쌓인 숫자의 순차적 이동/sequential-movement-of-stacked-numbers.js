bdee54f5b5298d1253b45fd2710028052f6d0a71
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-03-22 01:33:05 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 189ms, Memory: 15MB, Status: Passed - CodeTree

--- 250322/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.js
+++ 250322/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, m] = input[0].split(' ').map(Number);
-const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+const grid = input.slice(1, 1 + n).map(line => line.trim().split(' ').map(Number));
 const moveNums = input[1 + n].split(' ').map(Number);
 
 // Please Write your code here.
@@ -10,7 +10,7 @@ const findMoveNode = (x,y) => {
     const dx = [1,-1,0,0,1,-1,1,-1], dy = [0,0,1,-1,1,1,-1,-1];
 
     let maxval = -1;
-    let index = 0;
+    let index = -1;
     for(let i = 0; i<8; i++) {
         const nx = x + dx[i], ny = y + dy[i];
 
@@ -23,12 +23,16 @@ const findMoveNode = (x,y) => {
             }
         }
     }
+    if(index === -1) return [x,y]
+
     const nx = x + dx[index], ny = y + dy[index];
     return [nx,ny];
 }
 
 const Move = (x,y,nx,ny,val) => {
     // console.log(`move ${x},${y} to ${nx},${ny}`)
+    if(!grid[x][y].includes(val)) return;
+
     const index = grid[x][y].indexOf(val);
     const sliceArr = grid[x][y].slice(index);
 
@@ -40,45 +44,24 @@ for(let i = 0; i<n; i++)
     for(let j = 0; j<n; j++)
         grid[i][j] = [grid[i][j]]
 
-// moveNums.forEach(v => {
-//     let firstmove = true;
+moveNums.forEach(v => {
+    let firstmove = true;
 
-//     for(let i = 0; i<n; i++)
-//         for(let j = 0; j<n; j++)
-//             if(firstmove && grid[i][j].includes(v)) {
-//                 const [nx,ny] = findMoveNode(i,j);
-//                 if(grid[nx][ny].length > 0)
-//                     Move(i,j,nx,ny,v);
-//                 firstmove = false;
-//             }
-//     // console.log(grid)
-// })
-
-// console.log(grid)
+    for(let i = 0; i<n; i++)
+        for(let j = 0; j<n; j++)
+            if(firstmove && grid[i][j].includes(v)) {
+                const [nx,ny] = findMoveNode(i,j);
+                if(grid[nx][ny].length > 0 && !(nx === i && ny === j))
+                    Move(i,j,nx,ny,v);
+                firstmove = false;
+            }
+    // console.log(grid)
+})
 
 
-// grid.forEach(list => list.forEach(v => {
-//     if(v.length === 0)
-//         console.log('None')
-//     else
-//         console.log(v.reverse().join(' '))
-// }))
-
-const a = [1,2,3,4,5]
-const b = [6,7,8,9,0]
-
-const index = a.indexOf(3);
-const sliceArr = a.slice(index);
-
-b.push(...sliceArr);
-a.splice(index);
-
-console.log(a,b)
-console.log()
-const index2 = b.indexOf(3);
-const sliceArr2 = b.slice(index);
-
-a.push(...sliceArr2);
-b.splice(index2);
-
-console.log(a,b)
+grid.forEach(list => list.forEach(v => {
+    if(v.length === 0)
+        console.log('None')
+    else
+        console.log(v.reverse().join(' '))
+}))

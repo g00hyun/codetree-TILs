8f1ae67c9820a912f73b352c4707a6e9ef9b055a
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-03-22 00:43:03 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 165ms, Memory: 22MB, Status: Wrong Answer - CodeTree

---
+++ 250322/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+const moveNums = input[1 + n].split(' ').map(Number);
+
+// Please Write your code here.
+const findMoveNode = (x,y) => {
+    const dx = [1,-1,0,0,1,-1,1,-1], dy = [0,0,1,-1,1,1,-1,-1];
+
+    let maxval = -1;
+    let index = 0;
+    for(let i = 0; i<8; i++) {
+        const nx = x + dx[i], ny = y + dy[i];
+
+        if(0<=nx && nx<n && 0<=ny && ny<n) {
+            const curval = Math.max(...grid[nx][ny])
+            
+            if(maxval < curval) {
+                maxval = curval;
+                index = i;
+            }
+        }
+    }
+
+    const nx = x + dx[index], ny = y + dy[index];
+    return [nx,ny];
+}
+
+const Move = (x,y,nx,ny,val) => {
+    const index = grid[x][y].indexOf(val);
+    const sliceArr = grid[x][y].slice(index);
+
+    grid[nx][ny].push(...sliceArr);
+    grid[x][y].splice(index);
+}
+
+for(let i = 0; i<n; i++)
+    for(let j = 0; j<n; j++)
+        grid[i][j] = [grid[i][j]]
+
+moveNums.forEach(v => {
+    let firstmove = true;
+
+    for(let i = 0; i<n; i++)
+        for(let j = 0; j<n; j++)
+            if(firstmove && grid[i][j].includes(v)) {
+                const [nx,ny] = findMoveNode(i,j);
+                Move(i,j,nx,ny,v);
+                firstmove = false;
+            }
+})
+
+grid.forEach(v => {
+    if(v.length === 0)
+        console.log('None')
+})

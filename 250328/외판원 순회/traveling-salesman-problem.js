d9c1bc5f3f2ed3f7090dae7923bcef11f15ef8ad
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-03-28 23:14:38 +0900

    Title: 외판원 순회, Time: 181ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250328/외판원 순회/traveling-salesman-problem.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const cost = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+const q = [];
+const rowVisited = Array(n).fill(false);
+const colVisited = Array(n).fill(false);
+let answer = Number.MAX_SAFE_INTEGER;
+
+const Backtracking = (row) => {
+    if(q.length === n) {
+        answer = Math.min(answer, q.reduce((a,b) => a+b));
+        return;
+    }
+
+    if(rowVisited[row]) return;
+
+    for(let i = 0; i<n; i++) {
+        if(colVisited[i]) continue;
+
+        rowVisited[row] = true;
+        colVisited[i] = true;
+        q.push(cost[row][i]);
+        Backtracking(i);
+        colVisited[i] = false;
+        q.pop();
+    }
+
+    rowVisited[row] = false;
+}
+
+Backtracking(0)
+console.log(answer)

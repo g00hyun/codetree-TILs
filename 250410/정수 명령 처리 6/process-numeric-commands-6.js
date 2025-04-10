87fbd57bbf976960e1900bf62fb2285d1a963dc1
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-04-10 22:31:58 +0900

    Title: 정수 명령 처리 6, Time: 326ms, Memory: 22MB, Status: Passed - Codetree

--- 250410/정수 명령 처리 6/process-numeric-commands-6.js
+++ 250410/정수 명령 처리 6/process-numeric-commands-6.js
@@ -1,5 +1,25 @@
-const h = require(collections/sorted-map)
+const h = require("collections/heap")
 
-const pq = new h()
+const pq = new h();
 
-const fs = require('fs')
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const commands = input.slice(1);
+
+commands.forEach(v => {
+    let [c, a] = v.split(' ');
+    if(a) a = Number(a);
+
+    if(c === 'push')
+        pq.push(a)
+    else if(c === 'pop')
+        console.log(pq.pop())
+    else if(c === 'size')
+        console.log(pq.length)
+    else if(c === 'empty')
+        console.log(pq.length === 0 ? 1 : 0)
+    else if(c === 'top')
+        console.log(pq.peek())
+})

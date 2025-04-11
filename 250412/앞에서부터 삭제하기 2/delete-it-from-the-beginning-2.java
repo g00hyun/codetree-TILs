5857d89243eca955a40adfeea4962438aa0afc6f
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-04-12 04:19:04 +0900

    Title: 앞에서부터 삭제하기 2, Time: 381ms, Memory: 26MB, Status: Passed - Codetree

--- 250412/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 250412/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -1,25 +1,33 @@
 import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
+
+        int[] prefixSum = new int[n];
+        prefixSum[n-1] = Integer.parseInt(arr[n-1]);
+        for(int i = n-2; i >= 0; i--)
+            prefixSum[i] = Integer.parseInt(arr[i]) + prefixSum[i+1];
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int[] arr = new int[n];
+        // for(int i = 0; i < n; i++)
+        //     arr[i] = sc.nextInt();
         // Please write your code here.
         double maxAvg = -1;
-        for(int i = 1; i<=n-2; i++) {
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
-            for(int j = i; j<n; j++) {
-                pq.add(arr[j]);
-            }
-            pq.poll();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(Integer.parseInt(arr[n-1]));
 
-            int sum = 0;
-            for(int val : pq)
-                sum += val;
+        for(int i = n-2; i>0; i--) {
+            pq.add(Integer.parseInt(arr[i]));
 
-            maxAvg = Math.max(maxAvg, sum / (n-i-1));
+            int sum = prefixSum[i];
+            int min = pq.poll();
+            pq.add(min);
+
+            maxAvg = Math.max(maxAvg, (double)(sum - min) / (n-i-1));
         }
 
         String result = String.format("%.2f", maxAvg);

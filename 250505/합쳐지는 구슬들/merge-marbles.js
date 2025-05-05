887ef013b50a60ca6ea5658c2c1190f52e48bef0
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-05-05 19:20:54 +0900

    Title: 합쳐지는 구슬들, Time: 179ms, Memory: 15MB, Status: Passed - Codetree

--- 250505/합쳐지는 구슬들/merge-marbles.js
+++ 250505/합쳐지는 구슬들/merge-marbles.js
@@ -1,138 +1,119 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const EMPTY = [0, 0, 0];
-
-const dirMapper = {
-    'U': 0,
-    'R': 1,
-    'L': 2,
-    'D': 3,
-}
-
-// 변수 선언 및 입력:
 const [n, m, t] = input[0].split(' ').map(Number);
-const grid = Array.from(Array(n + 1), () => Array(n + 1).fill(EMPTY));
-const nextGrid = Array.from(Array(n + 1), () => Array(n + 1).fill(EMPTY));
-
-function inRange(x, y) {
-    return 0 <= x && x < n && 0 <= y && y < n;
-}
-
-function nextPos(x, y, moveDir) {
-    // 방향 전환을 쉽게 하기 위해 
-    // dx, dy 테크닉에서 0<->3, 1<->2가
-    // 서로 반대 방향이 되도록 정의합니다.
-    const dxs = [-1, 0, 0, 1];
-    const dys = [0, 1, -1, 0];
-
-    let nx = x + dxs[moveDir];
-    let ny = y + dys[moveDir];
-
-    // 벽에 부딪히게 된다면, 방향만 전환해줍니다.
-    if (!inRange(nx, ny)) {
-        moveDir = 3 - moveDir;
-    } 
-    // 그렇지 않다면, 한 칸 전진합니다.
-    else {
-        x = nx;
-        y = ny;
-    }
-
-    return [x, y, moveDir];
-}
-
-// (x, y) 위치에 새로운 구슬이 들어왔을 때 갱신을 진행합니다.
-function update(x, y, newMarble) {
-    // 기존 구슬 정보입니다.
-    let [num, weight, moveDir] = nextGrid[x][y];
-
-    // 새롭게 들어온 구슬 정보입니다.
-    let [newNum, newWeight, newDir] = newMarble;
-
-    // 새로 들어온 구슬이 더 우선순위가 높다면
-    // 번호와 방향은 새로운 구슬을 따르게 됩니다.
-    if (newNum > num)
-        nextGrid[x][y] = [newNum, weight + newWeight, newDir];
-    // 기존 구슬의 우선순위가 더 높다면
-    // 무게만 더해집니다.
-    else
-        nextGrid[x][y] = [num, weight + newWeight, moveDir];
-}
-
-function move(x, y) {
-    let [num, weight, moveDir] = grid[x][y];
-
-    // Step1. 현재 구슬의 다음 위치와 방향을 구합니다.
-    let [nx, ny, nextDir] = nextPos(x, y, moveDir);
-
-    // Step2. 구슬을 옮겨줍니다.
-    update(nx, ny, [num, weight, nextDir]);
-}
-
-function simulate() {
-    // Step1. nextGrid를 초기화합니다.
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            nextGrid[i][j] = EMPTY;
-        }
-    }
-
-    // Step2. 각 구슬들을 한 칸씩 움직여줍니다.
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] !== EMPTY) {
-                move(i, j);
-            }
-        }
-    }
-
-    // Step3. nextGrid 값을 grid로 옮겨줍니다.
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            grid[i][j] = nextGrid[i][j];
-        }
-    }
-}
-
-function getMarbleNum() {
-    let cnt = 0;
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] !== EMPTY) {
-                cnt += 1;
-            }
-        }
-    }
-    return cnt;
-}
-
-function getMaxWeight() {
-    let maxWeight = 0;
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] !== EMPTY) {
-                const [index, weight, direction] = grid[i][j];
-                maxWeight = Math.max(maxWeight, weight);
-            }
-        }
-    }
-    return maxWeight;
-}
-
+const marbles = [];
 for (let i = 1; i <= m; i++) {
     let [r, c, d, w] = input[i].split(' ');
-
-    [r, c, w] = [r, c, w].map(Number);
-    d = dirMapper[d];
-
-    grid[r - 1][c - 1] = [i, w, d];
+    r = Number(r);
+    c = Number(c);
+    w = Number(w);
+    marbles.push([r, c, d, w]);
 }
 
-// t초에 걸쳐 시뮬레이션을 진행합니다.
-for (let i = 1; i <= t; i++)
-    simulate();
+// Please Write your code here.
+const grid = Array(n).fill().map(v => Array(n).fill(null));
+const nextGrid = Array(n).fill().map(v => Array(n).fill([]));
 
-const marbleNum = getMarbleNum();
-const maxWeight = getMaxWeight();
-console.log(marbleNum, maxWeight);
+const InitGrid = () => {
+    marbles.forEach(([x,y,d,w],i) => {
+        grid[x-1][y-1] = [d,w,i+1]
+    })
+}
+
+const InitNextGrid = () => {
+    for(let i = 0; i<n; i++)
+        for(let j = 0; j<n; j++)
+            nextGrid[i][j] = [];
+}
+
+const dx = [0,0,-1,1], dy = [-1,1,0,0];
+const convert = {
+    'L': 0,
+    'R': 1,
+    'U': 2,
+    'D': 3
+}
+const reversal = {
+    'L': 'R',
+    'R': 'L',
+    'U': 'D',
+    'D': 'U'
+}
+
+const InRange = (x,y) => 0<=x && x<n && 0<=y && y<n;
+
+const AfterSec = () => {
+    InitNextGrid();
+
+    for(let i = 0; i<n; i++)
+        for(let j = 0; j<n; j++)
+            if(grid[i][j]) {
+                const x = i, y = j;
+                const [d,w,priority] = grid[i][j];
+                const dir = convert[d];
+
+                const nx = x + dx[dir], ny = y + dy[dir];
+                if(InRange(nx,ny))
+                    nextGrid[nx][ny].push([d,w,priority]);
+                else
+                    nextGrid[x][y].push([reversal[d],w,priority]); 
+            }
+}
+
+const MergeMarble = () => {
+    for(let i = 0; i<n; i++)
+        for(let j = 0; j<n; j++)
+            if(nextGrid[i][j].length >= 2) {
+                const ms = nextGrid[i][j].map(v => v);
+                nextGrid[i][j] = [];
+                
+                ms.sort((a,b) => (a[2] - b[2]) * -1);
+                const nw = ms.map(v => v[1]).reduce((a,b) => a + b,0)
+                
+                const nval = [ms[0][0], nw, ms[0][2]];
+                nextGrid[i][j].push(nval);
+            }
+}
+
+const UpdateGrid = () => {
+    for(let i = 0; i<n; i++)
+        for(let j = 0; j<n; j++)
+            grid[i][j] = null;
+
+    for(let i = 0; i<n; i++)
+        for(let j = 0; j<n; j++)
+            if(nextGrid[i][j].length === 1)
+                grid[i][j] = nextGrid[i][j][0];
+                
+}
+
+const Solution = () => {
+    // 맵 만들고, 초기세팅
+    InitGrid();
+
+    let left = t;
+    // t초동안 진행 시키기
+    while(left--) {
+        // 1초동안 움직이거나, 방향을 바꾸거나
+        AfterSec();
+        // 움직이고 나서, 충돌한다면 해당 칸에 가장 큰 번호의 구슬 방향을 남기고 크기 합치기
+        MergeMarble();
+        UpdateGrid();
+    }
+
+    const countMarbles = grid.map(v => v.filter(v => v).length).reduce((a,b) => a+b);
+    const maxWeight = grid.map(v => v.filter(v => v).map(v => v[1])).map(v => {
+        const len = v.length;
+        if(len === 0)
+            return 0;
+        else if(len === 1)
+            return v[0];
+        else if(len >= 2)
+            return v.reduce((a,b) => Math.max(a,b));
+    }).reduce((a,b) => Math.max(a,b))
+
+    console.log(countMarbles + ' ' + maxWeight)
+}
+
+Solution();

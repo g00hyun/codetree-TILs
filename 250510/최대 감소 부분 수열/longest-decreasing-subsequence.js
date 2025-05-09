ba37e8f150d4976c7715992887314e7cc17e36c6
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-05-09 23:15:33 +0900

    Title: 최대 감소 부분 수열, Time: 181ms, Memory: 15MB, Status: Passed - Codetree

--- 250510/최대 감소 부분 수열/longest-decreasing-subsequence.js
+++ 250510/최대 감소 부분 수열/longest-decreasing-subsequence.js
@@ -5,9 +5,8 @@ const n = Number(input[0]);
 const arr = input[1].trim().split(' ').map(Number);
 
 // Please Write your code here.
-const dp = Array(n).fill(0);
+const dp = Array(n).fill(1);
 
-dp[0] = 1;
 for(let i = 0; i<n; i++)
     for(let j = 0; j<i; j++)
         if(arr[i] < arr[j])

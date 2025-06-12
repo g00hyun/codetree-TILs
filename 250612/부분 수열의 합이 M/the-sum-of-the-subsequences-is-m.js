18b24f01f43f896b71b1c97e8058e9d20af3499f
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-06-12 10:33:40 +0900

    Title: 부분 수열의 합이 M, Time: 202ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250612/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, m] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+// Please Write your code here.
+
+const MAX_INT = Number.MAX_SAFE_INTEGER
+const dp = Array(m+1).fill(MAX_INT)
+
+dp[0] = 0
+for(let i = 0; i<n; i++)
+    for(let j = m; j>0; j--)
+        if(j >= arr[i] && dp[j - arr[i]] !== MAX_INT)
+            dp[j] = dp[j - arr[i]]+1
+
+const answer = dp[m]
+console.log(answer === MAX_INT ? -1 : answer)

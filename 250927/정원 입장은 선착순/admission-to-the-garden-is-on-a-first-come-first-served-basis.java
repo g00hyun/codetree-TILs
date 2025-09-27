2c0b10ee582d16fd9c8ea892d5cf9300147b23fd
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-09-27 22:03:47 +0900

    Title: 정원 입장은 선착순, Time: 419ms, Memory: 22MB, Status: Wrong Answer - Codetree

--- 250927/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.java
+++ 250927/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.java
@@ -5,7 +5,8 @@ public class Main {
     static int n;
     static int time;
     static Person[] person;
-    static PriorityQueue<Person> pq = new PriorityQueue<>((a,b) -> a.num - b.num);
+    static PriorityQueue<Person> pq = new PriorityQueue<>((a,b) -> a.arrivalTime - b.arrivalTime);
+    static PriorityQueue<Person> waiting = new PriorityQueue<>((a,b) -> a.num - b.num);
 
     public static void main(String[] args) throws IOException {
         // Please write your code here.
@@ -17,48 +18,46 @@ public class Main {
             person[i] = new Person(i+1, line[0], line[1]);
         }
 
+        for(Person p : person)
+            pq.add(p);
+
         time = 0;
         simulation();
     }
 
+// pq -> waiting
+// 넘어가는 시점 : time보다 작으면 넘어가면 됌
     private static void simulation() {
         // 처음 시작하는 시간으로 시간 설정
         time = Arrays.stream(person).filter(v -> !v.visited).map(v -> v.arrivalTime).reduce((a,b) -> Math.min(a, b)).get();
         // 시간에 맞는 사람 pq에 삽입
-        while(!isAllEnqueue() || !pq.isEmpty()) {
-            enqueue();
-            enterPersonAndSpendTime();
+        while(!pq.isEmpty()) {
+            Person p = pq.peek();
+            if(p.arrivalTime <= time) {
+                waiting.add(pq.poll());
+                continue;
+            }
+
+            if(waiting.isEmpty()) {
+                time = p.arrivalTime;
+            }
+
+            while(!waiting.isEmpty()) {
+                Person enter = waiting.poll();
+                enter.enterTime = time;
+                time += enter.spendTime;
+            }
+        }
+
+        while(!waiting.isEmpty()) {
+            Person enter = waiting.poll();
+            enter.enterTime = time;
+            time += enter.spendTime;
         }
 
         int result = Arrays.stream(person).map(v -> v.enterTime - v.arrivalTime).reduce(Math::max).get();
         print(result + "");
-        // 한명씩 pq에서 빼면서 시간 계산
 
-    }
-
-    private static boolean isAllEnqueue() {
-        return Arrays.stream(person).allMatch(v -> v.visited);
-    }
-
-    private static void enterPersonAndSpendTime() {
-        if(pq.isEmpty()) {
-            time = Arrays.stream(person).filter(v -> !v.visited).map(v -> v.arrivalTime).reduce((a,b) -> Math.min(a, b)).get();  
-        }
-        else {
-            Person p = pq.poll();
-            p.enterTime = time;
-
-            time += p.spendTime;
-        }
-    }
-
-    private static void enqueue() {
-        for(Person p : person) {
-            if(!p.visited && p.arrivalTime <= time) {
-                p.visited = true;
-                pq.add(p);
-            }
-        }
     }
 
     private static void print(String s) {
